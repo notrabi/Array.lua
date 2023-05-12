9441625e836b28655bf40f94503ae024d6d13652
notrabi <127636988+notrabi@users.noreply.github.com>
2023-05-12 21:56:47 +0100

    Finished Basic Functions
    Converts from/to Number and Scientific.
    Add, Subtract, Multiply and Divide now all included.

--- Array.lua
+++ Array.lua
@@ -1,140 +1,105 @@
---[[
-
+local array = {[[
 Path: 184nTG9pIRwd2i1XC7EpDVXBra1xgJeSsfHGb
-
-
 ░█████╗░██████╗░██████╗░░█████╗░██╗░░░██╗░░░██╗░░░░░██╗░░░██╗░█████╗░
 ██╔══██╗██╔══██╗██╔══██╗██╔══██╗╚██╗░██╔╝░░░██║░░░░░██║░░░██║██╔══██╗
 ███████║██████╔╝██████╔╝███████║░╚████╔╝░░░░██║░░░░░██║░░░██║███████║
 ██╔══██║██╔══██╗██╔══██╗██╔══██║░░╚██╔╝░░░░░██║░░░░░██║░░░██║██╔══██║
 ██║░░██║██║░░██║██║░░██║██║░░██║░░░██║░░░██╗███████╗╚██████╔╝██║░░██║
 ╚═╝░░╚═╝╚═╝░░╚═╝╚═╝░░╚═╝╚═╝░░╚═╝░░░╚═╝░░░╚═╝╚══════╝░╚═════╝░╚═╝░░╚═╝
-
 Made for storing large numbers past 1.7e308. Maximum limit: 1.7e1.7e308
-
 Made: June 26 2022 // Last Updated: May 12 2023
-
 Credits: (User: How)
-
 imcrazy: Every****ingthing
-
 Array = {num,x10^x}
-
-]]
-
-local array = {}
-
+]]}
 function array.new(num:number)
-	
 	local i = 0
-	
 	if num >= 10 then
-		
 		repeat
-			
 			task.wait()
-			
 			num /= 10
 			i += 1
-			
 		until num < 10
-		
 	end
-	
 	return {num,i}
-	
 end
-
 function array.fromScientific(scientific:string)
-
 	local num = scientific:match("%d+%.?%d*")
 	local i = scientific:match("e(%-?%d+)")
-
 	return {num,i}
-
 end
-
 function array.toNumber(array)
-
 	return array[1] * (10 ^ array[2])
-
 end
-
 function array.toScientific(array)
-
 	local num = tostring(array[1])
 	local dec = "."
-
 	dec ..= math.floor(num) % 10
-
 	return num.."e"..array[2]
-
 end
-
 function array.add(a,b)
-
 	local num1 = a[1] * 10 ^ a[2]
 	local num2 = b[1] * 10 ^ b[2]
-
 	return array.new(num1 + num2)
-
 end
-
 function array.sub(a,b)
-	
 	local i = a[2]
 	local j = b[2]
-	
 	if i > j then
-		
 		for k = 1,i-j do
-			
 			b[1] /= 10
 			j += 1
-			
 		end
-		
 	elseif j > i then
-		
 		for k = 1,j-i do
-			
 			a[1] /= 10
 			i += 1
-			
 		end
-		
 	end
-	
 	local num = a[1]-b[1]
-	
 	if num < 1 then
-
 		repeat
-
 			task.wait()
-
 			num *= 10
 			i -= 1
-
 		until num > 0
-
 	elseif num >= 10 then
-
 		repeat
-			
 			task.wait()
-			
 			num /= 10
 			i += 1
-			
 		until num < 10
-		
 	end
-	
 	return {num,i}
-	
 end
-
+function array.mul(a,b)
+	local num = a[1]*b[1]
+	local x = a[2]+b[2]
+	if num >= 10 then
+		repeat
+			task.wait()
+			num /= 10
+			x += 1
+		until num < 10
+	end
+	return {num,x}
+end
+function array.div(a,b)
+	local num = a[1]/b[1]
+	local x = a[2]-b[2]
+	if num < 1 then
+		repeat
+			task.wait()
+			num *= 10
+			x -= 1
+		until num >= 1
+	elseif num >= 10 then
+		repeat
+			task.wait()
+			num /= 10
+			x += 1
+		until num < 10 
+	end 
+	return {num,x}
+end
 return array
-
---credits: me (the furry)
